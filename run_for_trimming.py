1cb9162fef5f6bfffce577bc9fc1ad6f0f6a6086
mmcilree <matthew.j.mcilree@gmail.com>
2025-04-30 14:51:47 +1000

    Some scripts for trimming.

---
+++ run_for_trimming.py
@@ -0,0 +1,23 @@
+
+import logging
+
+#logging.basicConfig(filename="minizinc-python.log", level=logging.DEBUG)
+
+from datetime import timedelta
+from pathlib import Path
+
+import minizinc
+
+from mzn_bench import Configuration, schedule
+
+
+schedule(
+    instances=Path("./instances_for_trimming.csv"),
+    timeout=timedelta(minutes=5), 
+    configurations=[
+        Configuration(name="Huub_Proof", solver=minizinc.Solver.lookup("huub"), other_flags={"--prove": "proof"}),
+    ],
+    output_dir=Path("../pbarber/test"),
+    nodelist=None,
+    debug=True
+)
